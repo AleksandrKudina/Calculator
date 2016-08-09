5351d19552b4f7215fdfda885e5a865e36047d2d
Aleksandr Kudina <k_sasha90@mail.ru>
2016-08-09 21:58:33 +0300

    Initial Commit

---
+++ Calculator/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  Calculator
+//
+//  Created by Александр Кудина on 09.08.16.
+//  Copyright © 2016 Aleksandr Kudina. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
