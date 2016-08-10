ef443f5f27b1a44ccdbb5c5c95720c5c762878e2
Aleksandr Kudina <k_sasha90@mail.ru>
2016-08-10 17:09:19 +0300

    Без рефакторинга

--- Calculator/main.m
+++ Calculator/main.m
@@ -7,11 +7,64 @@
 //
 
 #import <Foundation/Foundation.h>
+#include <stdio.h>
+
 
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
-        // insert code here...
-        NSLog(@"Hello, World!");
+        char str[80];
+        scanf("%[^\n]s", str);
+        NSString *tmpStr = [[NSString alloc] initWithBytes:str length: sizeof(str) encoding:NSASCIIStringEncoding];
+        NSArray *array = [tmpStr componentsSeparatedByString:@" "];
+        NSLog(@"%@",array[0]);
+        NSLog(@"%@",array[1]);
+        NSLog(@"%@",array[2]);
+        
+        float operand1 = [array[1] floatValue];
+        float operand2 = [array[2] floatValue];
+        float rezalt;
+        NSString *operator = array[0];
+        
+        
+        if([operator isEqualToString:@"-"]){
+            rezalt = operand1-operand2;
+            NSLog(@"%f", rezalt);
+        }
+        
+        else if([operator isEqualToString:@"+"]){
+            rezalt = operand1+operand2;
+            NSLog(@"%f", rezalt);
+        }
+        
+        else if([operator isEqualToString:@"*"]){
+            rezalt = operand1*operand2;
+            NSLog(@"%f", rezalt);
+        }
+        
+        else if([operator isEqualToString:@"/"]){
+            rezalt = operand1/operand2;
+            NSLog(@"%f", rezalt);
+        }
+        else if([operator isEqualToString:@"powf"]){
+            rezalt = powf(operand1, operand2);
+            NSLog(@"%f", rezalt);
+        }
+        
+        else if([operator isEqualToString:@"mod"]){
+            rezalt = (int)operand1 % (int)operand2;
+            NSLog(@"%f", rezalt);
+        }
+        
+        else if([operator isEqualToString:@"sqrt"]){
+            rezalt = powf(operand1, 1/operand2);
+            NSLog(@"%f", rezalt);
+        }
+        
+        else if([operator isEqualToString:@"%"]){
+            rezalt = operand1*100/operand2;
+            NSLog(@"%f", rezalt);
+        }
+        
     }
     return 0;
 }
