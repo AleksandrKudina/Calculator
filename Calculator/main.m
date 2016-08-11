d02e86ba10049784f1e63c04b573240d56976369
Aleksandr Kudina <k_sasha90@mail.ru>
2016-08-12 00:04:55 +0300

    Сделал функцию. Обработал исключения/

--- Calculator/main.m
+++ Calculator/main.m
@@ -10,68 +10,78 @@
 #include <stdio.h>
 
 
+float calcFanction(NSString *operator, float operand1, float operand2);
+
 int main(int argc, const char * argv[]) {
+    
     @autoreleasepool {
+        
+     //   while (YES)
+       //     {
        
-        char str[80];
+                char str[80];
         
-        scanf("%[^\n]s", str);
+                scanf("%[^\n]s", str);
         
-        NSString *tmpStr = [[NSString alloc] initWithBytes:str length: sizeof(str) encoding:NSASCIIStringEncoding];
+                NSString *tmpStr = [[NSString alloc] initWithBytes:str length: sizeof(str) encoding:NSASCIIStringEncoding];
         
-        NSArray *array = [tmpStr componentsSeparatedByString:@" "];
+                NSArray *array = [tmpStr componentsSeparatedByString:@" "];
         
-        NSLog(@"%@",array[0]);
-        NSLog(@"%@",array[1]);
-        NSLog(@"%@",array[2]);
+                float operand1 = [array[1] floatValue];
+                float operand2 = [array[2] floatValue];
         
-        float operand1 = [array[1] floatValue];
-        float operand2 = [array[2] floatValue];
-        float rezalt;
+                NSString *operator = array[0];
+                
+         //       if ([operator isEqualToString:@"end"])
+           //         break;
         
-        NSString *operator = array[0];
+                float rezalt = calcFanction(operator, operand1, operand2);
         
         
-        if([operator isEqualToString:@"-"]){
-            rezalt = operand1-operand2;
-            
-        }
-        
-        else if([operator isEqualToString:@"+"]){
-            rezalt = operand1+operand2;
-            
-        }
-        
-        else if([operator isEqualToString:@"*"]){
-            rezalt = operand1*operand2;
-            
-        }
-        
-        else if([operator isEqualToString:@"/"]){
-            rezalt = operand1/operand2;
-           
-        }
-        else if([operator isEqualToString:@"powf"]){
-            rezalt = powf(operand1, operand2);
-          
-        }
-        
-        else if([operator isEqualToString:@"mod"]){
-            rezalt = (int)operand1 % (int)operand2;
-          
-        }
-        
-        else if([operator isEqualToString:@"sqrt"]){
-            rezalt = powf(operand1, 1/operand2);
-            
-        }
-        
-        else if([operator isEqualToString:@"%"]){
-            rezalt = operand1*100/operand2;
-            
-        }
-        
-        NSLog(@"%f", rezalt);
+                NSLog(@"%f", rezalt);
+        //}
     }
+  
     return 0;
 }
+
+float calcFanction(NSString *operator, float operand1, float operand2)
+{
+    @try{
+            if([operator isEqualToString:@"-"])
+                return operand1-operand2;
+        
+            else if([operator isEqualToString:@"+"])
+                return operand1+operand2;
+        
+            else if([operator isEqualToString:@"*"])
+                return operand1*operand2;
+        
+            else if([operator isEqualToString:@"/"])
+                return operand1/operand2;
+            
+            else if([operator isEqualToString:@"powf"])
+                return powf(operand1, operand2);
+        
+            else if([operator isEqualToString:@"mod"])
+                return (int)operand1 % (int)operand2;
+        
+            else if([operator isEqualToString:@"sqrt"])
+                return powf(operand1, 1/operand2);
+        
+            else if([operator isEqualToString:@"%"])
+                return operand1*100/operand2;
+
+        }
+    
+    @catch(NSException *e)
+        {
+            NSLog(@"Вы ввели неверные входные данные Ошибка:%@", e);
+        }
+    
+}
+
+
+
+
+
